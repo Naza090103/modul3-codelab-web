43f561680c701dc38c3dcd5cafb85a0eb0a4571a
Naza090103 <149454180+Naza090103@users.noreply.github.com>
2023-10-31 10:45:08 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const Hero = () => {
+  return <div>This is the Hero section</div>;
+};
+
+const Footer = () => {
+  return <footer>This is the Footer section</footer>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Hero />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
